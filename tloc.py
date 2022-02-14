220d5ab3a31fb7be676e9c95a872b24a26384482
lucassamir1 <lucaskvalcante@fisica.ufc.br>
2022-02-14 12:57:54 -0800

    vectorizing get interactions

--- tloc.py
+++ tloc.py
@@ -31,33 +31,47 @@ class Structure:
       dct = jsonio.decode(Path(filename).read_text())
       return dct
 
-   def get_distances(self):
+   def get_interactions(self):
+      #nmol number of molecules in the supercell
       nmol = self.nmuc * self.supercell[0] * self.supercell[1] \
       * self.supercell[2]
       
-      superlengthx = self.unitcell[0, 0] * self.supercell[0]
-      superlengthy = self.unitcell[1, 1] * self.supercell[1]
-
       # t number of translations and k directions
       translations_tk = np.mgrid[0:self.supercell[0]:1, 
                                  0:self.supercell[1]:1, 
                                  0:self.supercell[2]:1].reshape(3,-1).T
       transvecs_tk = np.matmul(translations_tk, self.unitcell)
+      t = len(transvecs_tk)
 
-      # n number of connections
-      nconnect = self.unique * len(transvecs_tk)
+      # nconnect number of connections
+      nconnect = self.unique * t
+
+      # connections between molecules
+      connec = (translations_tk[:, None] \
+         + self.uniqinter[:, 2:5][None, :]).reshape(nconnect, 3)
+
+      # enforce PBC
+      connec[:, 0][connec[:, 0] == self.supercell[0]] = 0
+      connec[:, 1][connec[:, 1] == self.supercell[1]] = 0
+      connec[:, 2][connec[:, 2] == self.supercell[2]] = 0
+      
+      # translated interactions 
+      transinter = np.empty([nconnect, 3])
+      transinter[:, 0] = (np.arange(0, nmol, self.nmuc)[:, None] \
+         + self.uniqinter[:, 0][None, :]).reshape(nconnect)
+      transinter[:, 1] = np.tile(self.uniqinter[:, 1], t) \
+         + self.nmuc * np.matmul(connec,
+         [self.supercell[1] * self.supercell[2], self.supercell[2], 1])
+      transinter[:, 2] = np.tile(self.uniqinter[:, 2], t)
+
+      superlengthx = self.unitcell[0, 0] * self.supercell[0]
+      superlengthy = self.unitcell[1, 1] * self.supercell[1]
 
       # translated coordinates for m molecules
       transcoords_mtk = self.coordmol[:, None] + transvecs_tk[None, :]
 
-      # translated interactions
-      transinter = np.empty([nmol, 3])
-      transinter[:, 0] = np.tile(self.uniqinter[:, 0], nconnect) \
-         + np.repeat(np.arange(0, nmol, self.nmuc), self.unique)
-      transinter[:, 1] = np.tile(self.uniqinter[:, 1], nconnect) \
-         + np.repeat(np.arange(0, nmol, self.nmuc), self.unique) \
-            + self.uniqinter[:, 2:5][None, :] * self.supercell
-      
+      # distance between molecules
+      distx = transcoords_mtk
 
    def squared_length(self):
-      nmol, nconnect, transinter, distx, disty = get_distances()
+      nmol, nconnect, transinter, distx, disty = get_interactions()
