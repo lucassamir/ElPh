1b8aa316160aaff93a444437e6c0673ddcb8110a
lucassamir1 <lucaskvalcante@fisica.ufc.br>
2022-02-13 18:10:54 -0800

    adding lattice function

--- tloc.py
+++ tloc.py
@@ -2,53 +2,62 @@ import numpy as np
 from pathlib import Path
 
 class Structure:
-    '''This class defines the charge mobility of molecular 
-       semiconductor through the Transient Localization Theory'''
+   def __init__(self, lattice_file, params_file, 
+   nmuc=None, coordmol=None, unitcell=None, 
+   supercell=None, unique=None, uniqinter=None,
+   javg=None, jdelta=None, nrepeat=None,
+   iseed=None, invtau=None, temp=None):
 
-    def __init__(self, lattice, params):
-        '''Creates the structure object.
+      lat_dic = read_json(lattice_file)
+      self.nmuc = lat_dic['nmuc']
+      self.coordmol = np.array(lat_dic['coordmol'])
+      self.unitcell = np.array(lat_dic['unitcell'])
+      self.supercell = lat_dic['supercell']
+      self.unique = lat_dic['unique']
+      self.uniqinter = np.array(lat_dic['uniqinter'])
+
+      par_dic = read_json(params_file)
+      self.javg = par_dic['javg']
+      self.jdelta = par_dic['jdelta']
+      self.nrepeat = par_dic['nrepeat']
+      self.iseed = par_dic['iseed']
+      self.invtau = par_dic['invtau']
+      self.temp = par_dic['temp']
         
-        lattice: dict
-           Lattice parameters as well as the size of the supercell
-        params: dict
-           Hopping averages (J) and fluctuations (DeltaJ), 
-           temperature and inverse of tau_in
-        '''
-
-        lat_dic = read_json('lattice.json')
-        nmol = lat_dic['nmol']
-        coordmol = lat_dic['coordmol']
-        unitcell = lat_dic['unitcell']
-        supercell = lat_dic['supercell']
-        unique = lat_dic['unique']
-        interactions = lat_dic['interactions']
-
-        par_dic = read_json('params.json')
-        javg = par_dic['javg']
-        jdelta = par_dic['jdelta']
-        nrepeat = par_dic['nrepeat']
-        iseed = par_dic['iseed']
-        invtau = par_dic['invtau']
-        temp = par_dic['temp']
+   def write_json(filename, data):
+      Path(filename).write_text(jsonio.MyEncoder(indent=4).encode(data))
         
-    def write_json(filename, data):
-        """Creates json file with specified name and data.
- 
-        Args: 
-           filename (string): Name of json file. Must include .json.
-           data (dict): Dictionary with data.
-        """
-        Path(filename).write_text(jsonio.MyEncoder(indent=4).encode(data))
-        
-    def read_json(filename):
-        """Reads specified json file. 
+   def read_json(filename):
+      dct = jsonio.decode(Path(filename).read_text())
+      return dct
 
-        Args:
-           filename (str): Full name of json file.
-        Returns:
-           dict: Dictionary with data specified by json file.
-        """
-        dct = jsonio.decode(Path(filename).read_text())
-        return dct
+   def get_distances(self):
+      nmol = self.nmuc * self.supercell[0] * self.supercell[1] \
+      * self.supercell[2]
+      
+      superlengthx = self.unitcell[0, 0] * self.supercell[0]
+      superlengthy = self.unitcell[1, 1] * self.supercell[1]
 
-    def squared_length(self):
+      # t number of translations and k directions
+      translations_tk = np.mgrid[0:self.supercell[0]:1, 
+                                 0:self.supercell[1]:1, 
+                                 0:self.supercell[2]:1].reshape(3,-1).T
+      transvecs_tk = np.matmul(translations_tk, self.unitcell)
+
+      # n number of connections
+      nconnect = self.unique * len(transvecs_tk)
+
+      # translated coordinates for m molecules
+      transcoords_mtk = self.coordmol[:, None] + transvecs_tk[None, :]
+
+      # translated interactions
+      transinter = np.empty([nmol, 3])
+      transinter[:, 0] = np.tile(self.uniqinter[:, 0], nconnect) \
+         + np.repeat(np.arange(0, nmol, self.nmuc), self.unique)
+      transinter[:, 1] = np.tile(self.uniqinter[:, 1], nconnect) \
+         + np.repeat(np.arange(0, nmol, self.nmuc), self.unique) \
+            + self.uniqinter[:, 2:5][None, :] * self.supercell
+      
+
+   def squared_length(self):
+      nmol, nconnect, transinter, distx, disty = get_distances()
