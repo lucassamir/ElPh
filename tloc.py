e2b5fc194f532067eb119856b87938ccda768950
lucassamir <lsrcavalcante@ucdavis.edu>
2022-02-15 12:09:43 -0800

    add distances

--- tloc.py
+++ tloc.py
@@ -32,7 +32,7 @@ class Structure:
       return dct
 
    def get_interactions(self):
-      #nmol number of molecules in the supercell
+      # nmol number of molecules in the supercell
       nmol = self.nmuc * self.supercell[0] * self.supercell[1] \
       * self.supercell[2]
       
@@ -46,32 +46,45 @@ class Structure:
       # nconnect number of connections
       nconnect = self.unique * t
 
-      # connections between molecules
-      connec = (translations_tk[:, None] \
-         + self.uniqinter[:, 2:5][None, :]).reshape(nconnect, 3)
+      # mapping
+      mapcell_k = [self.supercell[1] * self.supercell[2], 
+                   self.supercell[2], 
+                   1]
+      map2same_t = self.nmuc * np.matmul(translations_tk, mapcell_k)
 
-      # enforce PBC
-      connec[:, 0][connec[:, 0] == self.supercell[0]] = 0
-      connec[:, 1][connec[:, 1] == self.supercell[1]] = 0
-      connec[:, 2][connec[:, 2] == self.supercell[2]] = 0
+      # u unique connections to other molecules and enforced PBC
+      connec_tuk = (translations_tk[:, None] \
+         + self.uniqinter[:, 2:5][None, :]).reshape(nconnect, 3)
+      connec_tuk[:, :, 0][connec_tuk[:, :, 0] == self.supercell[0]] = 0
+      connec_tuk[:, :, 1][connec_tuk[:, :, 1] == self.supercell[1]] = 0
+      connec_tuk[:, :, 2][connec_tuk[:, :, 2] == self.supercell[2]] = 0
+      map2others_tu = self.nmuc * np.matmul(connec_tuk, mapcell_k)
       
       # translated interactions 
-      transinter = np.empty([nconnect, 3])
-      transinter[:, 0] = (np.arange(0, nmol, self.nmuc)[:, None] \
+      transinter = np.empty([nconnect, 3], dtype='int')
+      transinter[:, 0] = (map2same_t[:, None] \
          + self.uniqinter[:, 0][None, :]).reshape(nconnect)
-      transinter[:, 1] = np.tile(self.uniqinter[:, 1], t) \
-         + self.nmuc * np.matmul(connec,
-         [self.supercell[1] * self.supercell[2], self.supercell[2], 1])
-      transinter[:, 2] = np.tile(self.uniqinter[:, 2], t)
+      transinter[:, 1] = (map2others_tu[:, None] \
+         + self.uniqinter[:, 1][None, :]).reshape(nconnect)
+      transinter[:, 2] = np.tile(self.uniqinter[:, 5], t)
+
+      # translated coordinates for m molecules
+      transcoords_mk = (transvecs_tk[:, None] \
+         + self.coordmol[None, :]).reshape(nmol, 3)
+
+      # distance between molecules and enforces PBC
+      distx = transcoords_mk[transinter[:, 0] - 1, 0] \
+         - transcoords_mk[transinter[:, 1] - 1, 0]
+      disty = transcoords_mk[transinter[:, 0] - 1, 1] \
+         - transcoords_mk[transinter[:, 1] - 1, 1]
 
       superlengthx = self.unitcell[0, 0] * self.supercell[0]
       superlengthy = self.unitcell[1, 1] * self.supercell[1]
 
-      # translated coordinates for m molecules
-      transcoords_mtk = self.coordmol[:, None] + transvecs_tk[None, :]
-
-      # distance between molecules
-      distx = transcoords_mtk
-
+      distx[distx > superlengthx / 2] -= superlengthx
+      distx[distx < -superlengthx / 2] += superlengthx
+      disty[disty > superlengthy / 2] -= superlengthy
+      disty[disty < -superlengthy / 2] += superlengthy
+      
    def squared_length(self):
       nmol, nconnect, transinter, distx, disty = get_interactions()
