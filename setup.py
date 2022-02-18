e97b8c9fb335a0967d15cc3b5dddac6ff6590781
lucassamir1 <lucaskvalcante@fisica.ufc.br>
2022-02-18 08:54:54 -0800

    correct bug with insert 0 in javd and jdelta

---
+++ setup.py
@@ -0,0 +1,36 @@
+import re
+from pathlib import Path
+from setuptools import setup, find_packages
+
+txt = Path('tloc/__init__.py').read_text()
+version = re.search("__version__ = '(.*)'", txt).group(1)
+
+long_description = Path('README.md').read_text()
+
+setup(
+    name='tloc',
+    version=version,
+    description='Transient Localization Theory',
+    #long_description=long_description,
+    #long_description_content_type='text/markdown',
+    author='Lucas Cavalcante',
+    author_email='lsrcavalcante@ucdavis.edu',
+    url='https://github.com/lucassamir/TLoc',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['numpy', 'scipy'],
+    extras_require={'docs': ['sphinx', 'sphinxcontrib-programoutput']},
+    entry_points='''
+        [console_scripts]
+        tloc=tloc.tloc:main
+    ''',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Physics'
+    ])
---
+++ tloc/tloc.py
@@ -0,0 +1,270 @@
+import numpy as np
+import json
+from scipy import linalg
+from pathlib import Path
+
+class Structure:
+   def __init__(self, lattice_file, params_file, 
+   nmuc=None, coordmol=None, unitcell=None, 
+   supercell=None, unique=None, uniqinter=None,
+   javg=None, jdelta=None, nrepeat=None,
+   iseed=None, invtau=None, temp=None):
+
+      with open(lattice_file + '.json', 'r') as json_file:
+         lat_dic = json.load(json_file)
+      self.nmuc = lat_dic['nmuc']
+      self.coordmol = np.array(lat_dic['coordmol'])
+      self.unitcell = np.array(lat_dic['unitcell'])
+      self.supercell = lat_dic['supercell']
+      self.unique = lat_dic['unique']
+      self.uniqinter = np.array(lat_dic['uniqinter'])
+
+      with open(params_file + '.json', 'r') as json_file:
+         par_dic = json.load(json_file)
+      self.javg = np.array(par_dic['javg'])
+      self.jdelta = np.array(par_dic['jdelta'])
+      self.nrepeat = par_dic['nrepeat']
+      self.iseed = par_dic['iseed']
+      self.invtau = par_dic['invtau']
+      self.temp = par_dic['temp']
+
+      # addind 0 for the case that molecules dont interact
+      self.javg = np.insert(self.javg, 0, 0)
+      self.jdelta = np.insert(self.jdelta, 0, 0)
+
+   def get_interactions(self):
+      # nmol number of molecules in the supercell
+      nmol = self.nmuc * self.supercell[0] * self.supercell[1] \
+      * self.supercell[2]
+      
+      # t number of translations and k directions
+      translations_tk = np.mgrid[0:self.supercell[0]:1, 
+                                 0:self.supercell[1]:1, 
+                                 0:self.supercell[2]:1].reshape(3,-1).T
+      transvecs_tk = np.dot(translations_tk, self.unitcell)
+      t = len(transvecs_tk)
+
+      # nconnect number of connections
+      nconnect = self.unique * t
+
+      # mapping
+      mapcell_k = [self.supercell[1] * self.supercell[2], 
+                   self.supercell[2], 
+                   1]
+      map2same_t = self.nmuc * np.dot(translations_tk, mapcell_k)
+
+      # u unique connections to other molecules and enforced PBC
+      connec_tuk = (translations_tk[:, None] \
+         + self.uniqinter[:, 2:5][None, :])
+      connec_tuk[:, :, 0][connec_tuk[:, :, 0] == self.supercell[0]] = 0
+      connec_tuk[:, :, 1][connec_tuk[:, :, 1] == self.supercell[1]] = 0
+      connec_tuk[:, :, 2][connec_tuk[:, :, 2] == self.supercell[2]] = 0
+      map2others_tu = self.nmuc * np.dot(connec_tuk, mapcell_k)
+      
+      # translated interactions between a pair of molecules
+      transinter_mm = np.zeros([nmol, nmol], dtype='int')
+      firstmol = (map2same_t[:, None] \
+         + self.uniqinter[:, 0][None, :]).reshape(nconnect)
+      secondmol = (map2others_tu[:, None] \
+         + self.uniqinter[:, 1][None, :]).reshape(nconnect)
+      typeinter = np.tile(self.uniqinter[:, 5], t)
+      transinter_mm[firstmol - 1, secondmol - 1] = typeinter
+
+      # enforce hermitian
+      transinter_mm[secondmol - 1, firstmol - 1] = typeinter
+
+      # translated coordinates for m molecules
+      transcoords_mk = (transvecs_tk[:, None] \
+         + self.coordmol[None, :]).reshape(nmol, 3)
+
+      # distances between all molecules THAT INTERACT and enforces PBC
+      distx_mm = transcoords_mk[:, 0, None] - transcoords_mk[None, :, 0]
+      disty_mm = transcoords_mk[:, 1, None] - transcoords_mk[None, :, 1]
+
+      index = np.where(transinter_mm == 0)
+      distx_mm[index] = 0
+      disty_mm[index] = 0
+
+      superlengthx = self.unitcell[0, 0] * self.supercell[0]
+      superlengthy = self.unitcell[1, 1] * self.supercell[1]
+
+      distx_mm[distx_mm > superlengthx / 2] -= superlengthx
+      distx_mm[distx_mm < -superlengthx / 2] += superlengthx
+      disty_mm[disty_mm > superlengthy / 2] -= superlengthy
+      disty_mm[disty_mm < -superlengthy / 2] += superlengthy
+
+      return nmol, transinter_mm, distx_mm, disty_mm
+
+   def get_hamiltonian(self, nmol, transinter_mm):
+      rnd1_mm = np.random.rand(nmol, nmol)
+      rnd1_mm = (rnd1_mm + rnd1_mm.T) / 2 # enforce hermitian
+      rnd2_mm = np.random.rand(nmol, nmol)
+      rnd2_mm = (rnd2_mm + rnd2_mm.T) / 2 # enforce hermitian
+
+      log_mm = -2 * np.log(1 - rnd1_mm)
+      cos_mm = np.sqrt(log_mm) * np.cos(2 * np.pi * rnd2_mm)
+      # sin = np.sqrt(log) * np.sin(2 * np.pi * rnd2)
+
+      hamiltonian_mm = self.javg[transinter_mm] + self.jdelta[transinter_mm] * cos_mm
+      # y = javg + (jdelta * sin)     
+    
+      return hamiltonian_mm
+
+   def get_energies(self, nmol, transinter_mm):
+      hamiltonian_mm = self.get_hamiltonian(nmol, transinter_mm)
+
+      energies_m, vectors_mm = linalg.eigh(hamiltonian_mm)
+      
+      return energies_m.real, vectors_mm
+
+   def get_squared_length(self):
+      nmol, transinter_mm, distx_mm, disty_mm = self.get_interactions()
+
+      energies_m, vectors_mm = self.get_energies(nmol, transinter_mm)
+
+      operatorx_mm = (energies_m[:, None] - energies_m[None, :]) * \
+         np.dot(vectors_mm, np.dot(distx_mm, vectors_mm))
+
+      operatory_mm = (energies_m[:, None] - energies_m[None, :]) * \
+         np.dot(vectors_mm, np.dot(disty_mm, vectors_mm))
+
+      partfunc_m = np.exp(energies_m / self.temp)
+      partfunc = sum(partfunc_m)
+
+      sqlx = sum(sum(partfunc_m * (operatorx_mm**2 * 2 / (self.invtau**2 + \
+         (energies_m[:, None] - energies_m[None, :])**2))))
+      sqly = sum(sum(partfunc_m * (operatory_mm**2 * 2 / (self.invtau**2 + \
+         (energies_m[:, None] - energies_m[None, :])**2))))
+
+      sqlx /= partfunc
+      sqly /= partfunc
+
+      return sqlx, sqly
+
+   def get_disorder_avg_sql(self):
+      dsqlx, dsqly = 0, 0
+
+      print('Calculating average of squared transient localization')
+      for i in range(1, self.nrepeat + 1):
+         sqlx, sqly = self.get_squared_length()
+         #tsqlx = self.get_therm_avg(sqlx)
+         #tsqly = self.get_therm_avg(sqly)
+         #partfunc = self.get_therm_avg()
+
+         #dsqlx = (1 - 1 / i) * dsqlx + (tsqlx / partfunc) / i
+         #dsqly = (1 - 1 / i) * dsqly + (tsqly / partfunc) / i
+
+         #moving average
+         dsqlx -= dsqlx / i
+         dsqlx += sqlx / i
+
+         dsqly -= dsqly / i
+         dsqly += sqly / i
+
+         print(i, dsqlx, dsqly)
+
+      return dsqlx, dsqly
+   
+   def get_mobility(self):
+      dsqlx, dsqly = self.get_disorder_avg_sql()
+
+      mobx = (1 / self.temp) * self.invtau * dsqlx / 2
+      moby = (1 / self.temp) * self.invtau * dsqly / 2
+
+      return mobx, moby
+
+def write_lattice_file():
+   lattice = {'nmuc':2,
+              'coordmol':[[0.0, 0.0, 0.0], [0.5, 0.5, 0.0]],
+              'unitcell':[[1.0, 0.0, 0.0], [0.0, 1.7321, 0.0], [0.0, 0.0, 1000.0]],
+              'supercell':[16, 16, 1],
+              'unique':6,
+              'uniqinter':[[1, 1, 1, 0, 0, 1], 
+              [2, 2, 1, 0, 0, 1], 
+              [1, 2, 0, 0, 0, 3], 
+              [2, 1, 1, 0, 0, 2], 
+              [2, 1, 0, 1, 0, 2], 
+              [2, 1, 1, 1, 0, 3]]
+   }
+   with open('lattice.json', 'w', encoding='utf-8') as f:
+      json.dump(lattice, f, ensure_ascii=False, indent=2)
+
+def write_params_file():
+   params = {'javg':[-0.98296, 0.12994, 0.12994],
+             'jdelta':[0.49148, 0.06497, 0.06497],
+             'nrepeat':50,
+             'iseed':3987187,
+             'invtau':0.05,
+             'temp':0.25
+   }
+   with open('params.json', 'w', encoding='utf-8') as f:
+      json.dump(params, f, ensure_ascii=False, indent=4)
+
+def main(args=None):
+   import argparse
+
+   description = "Transient Localization Theory command line interface"
+
+   example_text = """examples:
+
+   Calculate charge mobility with:
+      tloc --mobility
+   """
+
+   formatter = argparse.RawDescriptionHelpFormatter
+   parser = argparse.ArgumentParser(description=description,
+                                    epilog=example_text, 
+                                    formatter_class=formatter)
+
+   help = """
+   All calculations require a lattice JSON 
+   file with the following properties:
+
+   lattice.json:
+
+   """
+   parser.add_argument('--lattice_file', nargs='*', help=help,
+                        default='lattice', type=str)
+
+   help = """
+   All calculations require a params json 
+   file with the following properties:
+
+   params.json:
+   
+   """
+   parser.add_argument('--params_file', nargs='*', help=help,
+                        default='params', type=str)
+
+   help = ("write example of lattice and params files")
+   parser.add_argument('--write_files', action='store_true' , help=help)
+
+   help = ("Calculate charge mobility")
+   parser.add_argument('--mobility', action='store_true' , help=help)
+
+   args = parser.parse_args(args)
+
+   if args.write_files:
+      write_lattice_file()
+      write_params_file()
+      return
+
+   if not Path(args.lattice_file + '.json').is_file():
+      msg = 'Lattice file could not be found'
+      raise FileNotFoundError(msg)
+
+   if not Path(args.params_file + '.json').is_file():
+      msg = 'Params file could not be found'
+      raise FileNotFoundError(msg)
+
+   print('Initializing molecular structure')
+   st = Structure(args.lattice_file, args.params_file)
+
+   if args.mobility:
+      mobx, moby = st.get_mobility()
+      print('Calculating charge mobility')
+      print('mu_x = ', mobx)
+      print('mu_y = ', moby)
+
+if __name__  == '__main__':
+   main()
