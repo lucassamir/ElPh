ab2b80641b2d3f072a094516bcbaad2157689e4c
lucassamir1 <lucaskvalcante@fisica.ufc.br>
2022-03-07 14:11:58 -0800

    creating function that calculates jdelta

---
+++ tloc/jdelta.py
@@ -0,0 +1,60 @@
+import numpy as np
+from ase.calculators.gaussian import Gaussian
+from ase.io import read
+
+def get_orbitals(atoms, name):
+    atoms.calc = Gaussian(mem='2GB',
+                          nprocshared=1,
+                          label=name,
+                          save=None,
+                          method='b3lyp',
+                          basis='6-31G*',
+                          scf='tight',
+                          extra='nosymm punch=mo iop(3/33=1)')
+    atoms.get_potential_energy()
+
+def derivative(i, k, delta1, delta2):
+    dj = []
+    for delta in [delta1, delta2]:
+        for sign in [-1, 1]:
+            prefix = 'dj-{}-{}{}{}' .format(delta, i,
+                                            'xyz'[k],
+                                            ' +-'[sign])
+            with open(prefix + '.txt') as f:
+                dj.append(f.read())
+    return (-dj[3] + 8 * dj[1] - 8 * dj[0] + dj[2]) / (12 * delta1)
+
+def get_dj_matrix(delta1, delta2):
+    atoms = read('static')
+    i = len(atoms)
+    dj_ik = np.zeros([i, 3])
+    for ii in range(i):
+        for k in range(3):
+            dj_ik[ii, k] = derivative(ii, k, delta1, delta2)
+
+def finite_dif(delta=0.01):
+    atoms = read('static')
+    pos_ik = atoms.get_positions()
+    indices = list(range(len(atoms)))
+
+    for i in indices:
+        for k in range(3):
+            for sign in [-1, 1]:
+                # Update atomic positions
+                atoms.positions = pos_ik
+                atoms.positions[i, k] = pos_ik[i, k] + sign * delta
+                prefix = 'dj-{}-{}{}{}' .format(delta, i,
+                                                'xyz'[k],
+                                                ' +-'[sign])
+                name = prefix + '.com'
+                get_orbitals(atoms, name)
+
+def jdelta(delta=0.01):
+    d1 = delta / 2
+    d2 = delta
+    finite_dif(d1)
+    finite_dif(d2)
+    get_dj_matrix(d1, d2)
+
+if __name__ == '__main__':
+    jdelta(delta=0.01)
