3a6b1f6cba24c9dac5624e53da9e932a576e79ed
lucassamir <lucaskvalcante@fisica.ufc.br>
2022-03-08 10:36:50 -0800

    change fort.7 file name

--- tloc/jdelta.py
+++ tloc/jdelta.py
@@ -1,17 +1,20 @@
 import numpy as np
 from ase.calculators.gaussian import Gaussian
 from ase.io import read
+from shutil import move
 
 def get_orbitals(atoms, name):
-    atoms.calc = Gaussian(mem='2GB',
-                          nprocshared=1,
+    atoms.calc = Gaussian(mem='4GB',
+                          nprocshared=24,
                           label=name,
                           save=None,
                           method='b3lyp',
                           basis='6-31G*',
                           scf='tight',
+                          pop='full',
                           extra='nosymm punch=mo iop(3/33=1)')
     atoms.get_potential_energy()
+    move('fort.7', name)
 
 def derivative(i, k, delta1, delta2):
     dj = []
@@ -25,7 +28,7 @@ def derivative(i, k, delta1, delta2):
     return (-dj[3] + 8 * dj[1] - 8 * dj[0] + dj[2]) / (12 * delta1)
 
 def get_dj_matrix(delta1, delta2):
-    atoms = read('static')
+    atoms = read('static.xyz')
     i = len(atoms)
     dj_ik = np.zeros([i, 3])
     for ii in range(i):
@@ -33,7 +36,7 @@ def get_dj_matrix(delta1, delta2):
             dj_ik[ii, k] = derivative(ii, k, delta1, delta2)
 
 def finite_dif(delta=0.01):
-    atoms = read('static')
+    atoms = read('static.xyz')
     pos_ik = atoms.get_positions()
     indices = list(range(len(atoms)))
 
@@ -46,8 +49,7 @@ def finite_dif(delta=0.01):
                 prefix = 'dj-{}-{}{}{}' .format(delta, i,
                                                 'xyz'[k],
                                                 ' +-'[sign])
-                name = prefix + '.com'
-                get_orbitals(atoms, name)
+                get_orbitals(atoms, prefix)
 
 def jdelta(delta=0.01):
     d1 = delta / 2
