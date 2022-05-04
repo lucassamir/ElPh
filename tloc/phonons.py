2d3a1f3db1122c4e6bc2d55f1ef0c79c470fb280
lucassamir <lucaskvalcante@fisica.ufc.br>
2022-05-04 11:24:04 -0700

    checking units

--- tloc/phonons.py
+++ tloc/phonons.py
@@ -13,7 +13,7 @@ def write_phonons(mesh=[8, 8, 8], phonopy_file="phonopy_params.yaml"):
 
     # converting energy unit
     thz2ev = 4.13566733e-3 
-    freqs_e *= thz2ev * (2 * np.pi) # eV
+    freqs_e *= thz2ev # eV
 
     # nqpoints x (natoms x 3 directions) x nbands from phonopy
     vecs = phonon._mesh.eigenvectors
