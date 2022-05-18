bbc441af0090bb5ca051d46aa58747f5416c2146
lucassamir1 <lucaskvalcante@fisica.ufc.br>
2022-05-17 17:57:53 -0700

    using eigendisplacements

--- tloc/phonons.py
+++ tloc/phonons.py
@@ -12,22 +12,29 @@ def write_phonons(mesh=[8, 8, 8], phonopy_file="phonopy_params.yaml"):
     # e modes 
     freqs_e = freqs.flatten()
 
-    # masses
-    masses = phonon._mesh._cell.masses
-
     # converting energy unit
     thz2ev = 4.13566733e-3 
     freqs_e *= thz2ev # eV
 
     # nqpoints x (natoms x 3 directions) x nbands from phonopy
     vecs = phonon._mesh.eigenvectors
+
+    # masses, frac_coords and qpoints
+    masses_a = phonon._mesh._cell.masses
+    fcoords_av = phonon._mesh._cell.get_scaled_positions()
+    qpoints_qv = phonon._mesh.qpoints
+
+    # transform eigenvectors to eigendisplacements
+    factor_qa = np.exp(2j * np.pi * np.dot(fcoords_av[None, :], qpoints_qv[:, None])) / np.sqrt(masses_a[None, :])
+    vecs = np.repeat(factor_qa, 3).reshape(len(qpoints_qv), 3 * len(masses_a))[:, :, None] * vecs
     
     # e modes, a atoms, v directions 
     vecs = np.transpose(vecs, axes=[0, 2, 1])
-    vecs_eav = vecs.real.reshape(len(freqs_e), -1, 3)
+    #vecs_eav = vecs.real.reshape(len(freqs_e), -1, 3)
+    vecs_eav = np.absolute(vecs.reshape(len(freqs_e), -1, 3))
 
     # mass weighted
-    vecs_eav /= np.sqrt(masses)[None, :, None]
+    #vecs_eav /= np.sqrt(masses)[None, :, None]
 
     # avoid negative (imaginary) frequencies
     ind = np.where(freqs_e > 0)
