35974ff69bc64e067625f03e2f56dbf5861ebe4d
lucassamir <lucaskvalcante@fisica.ufc.br>
2022-06-02 13:55:30 -0700

    not taking the absolute of DeltaJ

--- tloc/jdelta.py
+++ tloc/jdelta.py
@@ -114,7 +114,7 @@ def get_dj_matrix(jlists, delta):
 
     dj_matrix = (np.abs(jp) - np.abs(jm)) / (2 * delta)
 
-    return np.abs(dj_matrix)
+    return dj_matrix
 
 def get_deviation(pair_atoms, dj_av, temp):
     freqs_e, vecs_eav, nq = load_phonons(pair_atoms)
