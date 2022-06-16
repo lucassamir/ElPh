c34da2bc1dd8e7e022ba313a3a867bc4f3c43bfb
lucassamir <lucaskvalcante@fisica.ufc.br>
2022-06-16 15:37:30 -0700

    docs for view

--- tloc/visualization.py
+++ tloc/visualization.py
@@ -3,6 +3,15 @@ import json
 from tloc.jdelta import load_phonons, get_dj_matrix
 
 def heat_atoms(molpair, sigma_eav):
+    """Scatter plot of atoms with heat indicating 
+    the highest contributions to sigma. Save plot data 
+    (atomic positions, atomic sizes and sigma contribution) 
+    to numpy compressed file.
+
+    Args:
+        molpair (str): Molecular pair label
+        sigma_eav (array): Sigma array of sizes modes, atoms and directions
+    """
     from ase.io import read
     import matplotlib.pyplot as plt
     # import plotly.graph_objects as go
@@ -45,6 +54,18 @@ def heat_atoms(molpair, sigma_eav):
     # fig.show()
 
 def heat_modes(molpair, sigma_eav, vecs_eav, n):
+    """Scatter plot of atoms with arrows indicating
+    phonon displacements of n phonons modes with the 
+    highest sigma contributions. Save plot data 
+    (atomic positions, vector directions, atomic sizes 
+    and sigma contribution) to numpy compressed file.
+
+    Args:
+        molpair (str): Molecular pair label
+        sigma_eav (array): Sigma array of sizes modes, atoms and directions
+        vecs_eav (array): Array of phonon eigendisplacements of size modes, atoms and directions
+        n (int): Top n phonon modes with highest sigma contribution
+    """
     from ase.io import read
     import matplotlib.pyplot as plt
 
@@ -77,6 +98,17 @@ def heat_modes(molpair, sigma_eav, vecs_eav, n):
         np.savez_compressed('view_modes_' + molpair + '_' + '{}' .format(i) + '_' + '.npz', **data)
 
 def sigma_contribution(pair_atoms, dj_av, temp):
+    """Standard deviation (sigma) in function of
+    modes, atoms and directions.
+
+    Args:
+        pair_atoms (list): List containing atom indices of molecular pair
+        dj_av (array): Delta of J in function of atoms and directions
+        temp (float): Temperature in eV
+
+    Returns:
+        typle: sigma and eigendisplacements in function of modes, atoms and directions
+    """
     freqs_e, vecs_eav, _ = load_phonons(pair_atoms)
     epcoup_eav = vecs_eav * dj_av[None, :, :]
     ssigma_eav = epcoup_eav**2 / (2 * np.tanh(freqs_e[:, None, None] / (2 * temp)))
@@ -84,6 +116,18 @@ def sigma_contribution(pair_atoms, dj_av, temp):
     return np.sqrt(ssigma_eav), vecs_eav
 
 def get_sigma(pair, delta, temp, mode, n):
+    """Read transfer integrals of displacements systems and calculate sigma
+
+    Args:
+        pair (dic): Dictionary containing molecular indices of pair
+        delta (float): Step size to displace atoms in Ang
+        temp (float): Temperature in eV
+        mode (str): Mode of visualization (atoms or modes)
+        n (int): Top n phonon modes with highest sigma contribution
+
+    Raises:
+        NotImplementedError: Error raised if the chosen mode is not atoms or modes
+    """
     mol1 = str(int(pair[1][0]) + 1)
     mol2 = str(int(pair[1][1]) + 1)
     molpair = pair[0]
@@ -107,6 +151,19 @@ def get_sigma(pair, delta, temp, mode, n):
         raise NotImplementedError(msg)
 
 def view(mode='atoms', n=3):
+    """Main function of the visualization module. There are two modes:
+
+    [atoms]: Scatter plot of atoms with heat indicating 
+    the highest contributions to sigma.
+
+    [modes] [[n]] : Scatter plot of atoms with arrows indicating
+    phonon displacements of n phonons modes with the 
+    highest sigma contributions
+
+    Args:
+        mode (str, optional): Mode of visualization (atoms or modes). Defaults to 'atoms'.
+        n (int, optional): Top n phonon modes with highest sigma contribution. Only useful for modes. Defaults to 3.
+    """
     with open('all_pairs.json', 'r') as json_file:
         pairs = json.load(json_file)
     
