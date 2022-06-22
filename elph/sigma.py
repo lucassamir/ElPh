99b716bbee923580fbc3fb0ef85ad70071a1f0d8
lucassamir1 <lucaskvalcante@fisica.ufc.br>
2022-06-22 15:28:08 -0700

    always using a 2x1x1 supercell

--- elph/sigma.py
+++ elph/sigma.py
@@ -27,10 +27,7 @@ def load_phonons(pair_atoms, phonon_file='phonon.npz', map_file='atom_mapping.js
     
     # use mapping to order the wrapped phonon modes
     # based on the unwrapped atoms
-    if len(mapping) > len(phonon['vecs'][0]):
-        vecs_eav = np.tile(phonon['vecs'], [1, 2, 1])[:, mapping, :]
-    else:
-        vecs_eav = phonon['vecs'][:, mapping, :]
+    vecs_eav = np.tile(phonon['vecs'], [1, 2, 1])[:, mapping, :]
 
     # selecting only the phonon modes relevant to the 
     # interaction pair of molecules
